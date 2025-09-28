8ccf1e0ebded61777be297b44bc802eac9a3442e
Vitor Lima <vitorauglima@gmail.com>
2025-09-28 20:23:29 -0300

    correção na letra-a

--- Letra-A.py
+++ Letra-A.py
@@ -3,33 +3,47 @@ import statistics
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def partition(arr, low, high):
-    
+def quickSort(arr, low, high, contador):
+    if low < high:
+        contador["comparacoes"] += 1
+        pi = partition(arr, low, high, contador)
+
+        quickSort(arr, low, pi - 1, contador)
+        quickSort(arr, pi + 1, high, contador)
+
+
+def partition(arr, low, high, contador):
+    # Pivô pela mediana de três (low, mid, high)
     pivot = arr[high]
-    
     i = low - 1
-    
+
     for j in range(low, high):
+        contador["comparacoes"] += 1
         if arr[j] < pivot:
             i += 1
-            swap(arr, i, j)
-    
-    swap(arr, i + 1, high)
+            swap(arr, i, j, contador)
+
+    swap(arr, i + 1, high, contador)
     return i + 1
 
-def swap(arr, i, j):
+def insertionSort(arr, low, high, contador):
+    for i in range(low + 1, high + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= low and arr[j] > key:
+            contador["comparacoes"] += 1
+            arr[j + 1] = arr[j]
+            contador["trocas"] += 1
+            j -= 1
+        contador["comparacoes"] += 1
+        arr[j + 1] = key
+
+def swap(arr, i, j, contador):
+    contador["trocas"] += 1
     arr[i], arr[j] = arr[j], arr[i]
 
-def quickSort(arr, low, high):
-    if low < high:
-        
-        pi = partition(arr, low, high)
-
-        quickSort(arr, low, pi - 1)
-        quickSort(arr, pi + 1, high)
-
 if __name__ == "__main__":
-    arquivo = "vetor_repetidos.txt"
+    arquivo = "vetor_ordenado.txt"
 
     with open(arquivo, "r") as f:
         arr_base = [int(x) for x in f.read().split()]
@@ -46,7 +60,7 @@ if __name__ == "__main__":
             arr = arr_base[:]
             contador = {"comparacoes": 0, "trocas": 0}
             start = time.time()
-            quickSort(arr, 0, n - 1, m, contador)
+            quickSort(arr, 0, n - 1, contador)
             end = time.time()
             tempos.append(end - start)
             comparacoes.append(contador["comparacoes"])
