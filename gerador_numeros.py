5d5f8c463d1d8910fd88f4786ceb50ad9c8f46cd
Rammid <rammid.barreto@gmail.com>
2025-10-02 12:06:17 -0300

    gerador de numeros atualizado

--- gerador_numeros.py
+++ gerador_numeros.py
@@ -6,22 +6,28 @@ def salvar_vetor(nome_arquivo, vetor):
 
 
 if __name__ == "__main__":
-    n = 500  # tamanho do vetor
+    #n = 500  # tamanho do vetor
 
-    # 1) Vetor aleatório
-    aleatorio = [random.randint(0, 1000) for _ in range(n)]
+    # 1) Vetor para achar M
+    Achar_M = [random.randint(0, 10000) for _ in range(1000)]
+    salvar_vetor("vetor_Achar_M.txt", Achar_M)
+
+    # 2) Vetor aleatório
+    aleatorio = [random.randint(0, 10000) for _ in range(5000)]
     salvar_vetor("vetor_aleatorio.txt", aleatorio)
 
-    # 2) Vetor já ordenado (crescente)
-    ordenado = list(range(n))
+    # 3) Vetor ordenado em ordem crescente (pior caso)
+    ordenado = random.sample(range(10000), 6000)
+    ordenado.sort()
     salvar_vetor("vetor_ordenado.txt", ordenado)
 
-    # 3) Vetor ordenado de forma inversa (decrescente)
-    inverso = list(range(n, 0, -1))
+    # 4) Vetor ordenado em ordem decrescente (pior caso)
+    inverso = random.sample(range(10000), 7000)
+    inverso.sort(reverse=True)
     salvar_vetor("vetor_inverso.txt", inverso)
 
-    # 4) Vetor com muitos elementos repetidos (apenas 5 valores possíveis)
-    repetidos = [random.choice([1, 2, 3, 4, 5]) for _ in range(n)]
+    # 5) Vetor com muitos elementos repetidos (apenas 5 valores possíveis)
+    repetidos = [random.choice([1, 2, 3, 4, 5]) for _ in range(8000)]
     salvar_vetor("vetor_repetidos.txt", repetidos)
 
 
